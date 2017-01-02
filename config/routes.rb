9449c18eecc3128f6611555a51c8596e4bd853cf
Lee Saville <l.w.saville@gmail.com>
2017-01-02 16:11:29 -0700

    Add goole oauth route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/google_oauth2', as: :google_login
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#destroy'
 end
