2a25a36798ead9c47a81c5feb778508076315e3b
Lee Saville <l.w.saville@gmail.com>
2017-01-02 15:54:42 -0700

    Add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'logout', to: 'sessions#destroy'
 end
