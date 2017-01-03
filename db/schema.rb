e95f565afdf85fdf3df7c60d625616e015b28c17
Lee Saville <l.w.saville@gmail.com>
2017-01-02 20:13:42 -0700

    Add avatar_url to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102225132) do
+ActiveRecord::Schema.define(version: 20170102231833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170102225132) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "avatar_url"
   end
 
 end
