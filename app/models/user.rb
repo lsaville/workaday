edbe3e4c274caf659fa123161620dd7c6b02c84b
Lee Saville <l.w.saville@gmail.com>
2017-01-02 20:09:55 -0700

    Fix typo in credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def.self.from_omniauth(auth)
+  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider    = auth.provider
       user.uid         = auth.uid
@@ -7,7 +7,7 @@ class User < ApplicationRecord
       user.email       = auth.info.email
       user.avatar_url  = auth.info.image
       user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentails.expires_at)
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
   end
