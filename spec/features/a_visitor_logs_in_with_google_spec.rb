9bb94da84a7808a826d76313d94099de1fde45ca
Lee Saville <l.w.saville@gmail.com>
2017-01-02 21:34:00 -0700

    Change order of operations in test flow

--- spec/features/a_visitor_logs_in_with_google_spec.rb
+++ spec/features/a_visitor_logs_in_with_google_spec.rb
@@ -10,15 +10,34 @@ describe 'visitor' do
     scenario 'they click login and are redirected' do
       visit root_path
 
-      expect(status).to eq(200)
-      expect(current_path).to eq(login_path)
-      
       click_on "Login or Create Account"
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(login_path)
+
+      click_on "Sign In With Google"
 
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Frank")
       expect(page).to have_content("Logout")
       expect(page).to_not have_content("Login")
     end
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '543243',
+      info: {
+              name: 'Frank Gonzalez',
+              email: 'frank@example.com',
+              image: 'https://robohash.org/dssgds'
+            },
+      credentials: {
+                    token: 'blahblah',
+                    expires_at: 3285252
+                   }
+    })
   end
 end
