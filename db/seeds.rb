06c8e491cd29e0ea395865c0443368248fa1c0d6
Lee Saville <l.w.saville@gmail.com>
2017-01-04 08:07:32 -0700

    Create oo seed file with users and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seeds
+  def self.start
+    seeds = Seeds.new
+    seeds.create_users
+    seeds.create_projects
+  end
+
+  def create_users
+    10.times do |i| 
+      user = User.create!(
+        provider: "google_oauth2",
+        uid: Faker::Number.number(10),
+        name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        oauth_token: Faker::Number.number(7),
+        oauth_expires_at: Faker::Time.forward(30, :morning)
+      )
+      puts "Created user: #{i}, name: #{user.name}"
+    end
+  end
+
+  def create_projects
+    10.times do |i|
+      project = Project.create!(
+        lat: Faker::Address.latitude,
+        lng: Faker::Address.longitude,
+        title: Faker::Book.title,
+        details: Faker::Hipster.paragraph,
+      )
+      puts "Created project: #{i}, title: #{project.title}"
+    end
+  end
+end
+
+Seeds.start
