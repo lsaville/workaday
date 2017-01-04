c21be088713d8d6ba1d41fa74049a9be73e214bb
Lee Saville <l.w.saville@gmail.com>
2017-01-03 20:04:55 -0700

    Add omniauth stub to logout properly

--- spec/features/as_a_logged_in_user_logging_out_spec.rb
+++ spec/features/as_a_logged_in_user_logging_out_spec.rb
@@ -1,18 +1,40 @@
 require 'rails_helper'
 
 describe 'user' do
+  before(:each) do
+    include Capybara::DSL
+    Capybara.app = Workaday::Application
+    stub_omniauth
+  end
+
   context 'already logged in' do
     scenario 'they log out' do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      visit dashboard_path
+      visit login_path
+      click_on "Sign In With Google"
 
       click_on "Logout"
 
-      expect(current_path).to be(login_path)
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Sign In With Google")
       expect(page).to have_content("Login or Create Account")
       expect(page).to_not have_content("Logout")
     end
+  end
+  
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '543243',
+      info: {
+              name: 'Frank Gonzalez',
+              email: 'frank@example.com',
+              image: 'https://robohash.org/dssgds'
+            },
+      credentials: {
+                    token: 'blahblah',
+                    expires_at: 3285252
+                   }
+    })
   end
 end
