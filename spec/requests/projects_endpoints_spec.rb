b1872cc0111c6d5290b8a085d3b1e9af3fa79ffb
Lee Saville <l.w.saville@gmail.com>
2017-01-04 07:45:36 -0700

    Fix syntax for accessing json object attributes, change expectation to want strings

--- spec/requests/projects_endpoints_spec.rb
+++ spec/requests/projects_endpoints_spec.rb
@@ -13,11 +13,11 @@ describe 'projects endpoint' do
 
       expect(response).to be_success
       expect(response_comments.count).to eq(3)
-      expect(first.lat).to eq(projects.first.lat)
-      expect(first.lng).to eq(projects.first.lng)
-      expect(first.title).to eq(projects.first.title)
-      expect(first.details).to eq(projects.first.details)
-      expect(last.title).to eq(projects.last.title)
+      expect(first['lat']).to eq(projects.first.lat.to_s)
+      expect(first['lng']).to eq(projects.first.lng.to_s)
+      expect(first['title']).to eq(projects.first.title.to_s)
+      expect(first['details']).to eq(projects.first.details.to_s)
+      expect(last['title']).to eq(projects.last.title.to_s)
     end
   end
 end
