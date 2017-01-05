76e2cba9936c22ccccb4e771d301084f3af2b6af
Lee Saville <l.w.saville@gmail.com>
2017-01-05 16:35:52 -0700

    Fix functions to be key value pairs

--- app/assets/javascripts/components/_map.js.jsx
+++ app/assets/javascripts/components/_map.js.jsx
@@ -4,20 +4,20 @@ var Map = React.createClass({
     this.marker = this.createMarker();
   },
 
-  var location = {
+  location: {
     lat: this.props.project.lat,
     lng: this.props.project.lng
-  };
-  
-  function createMap() {
+  },
+
+  createMap: function(){
     var mapOptions = {
       zoom: 12,
       center: location
     }
-    return new google.maps.Map(this.refs.mapCanvas, mapOptions); 
+    return new google.maps.Map(this.refs.map_canvas.getDOMNode(), mapOptions);
   },
 
-  function createMarker() {
+  createMarker: function(){
     var markerOptions = {
       position: location,
       map: this.map
@@ -25,21 +25,21 @@ var Map = React.createClass({
   },
 
   render(){
-    function initMap() {
-      var location = {lat: this.props.project.lat, lng: this.props.project.lng};
-      var map = new google.maps.Map(document.getElementById('map-small_' + this.project.id), {
-        zoom: 12,
-        center: location
-      });
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
-      });
-    }
+//    function initMap() {
+//      var location = {lat: this.props.project.lat, lng: this.props.project.lng};
+//      var map = new google.maps.Map(document.getElementById('map-small_' + this.project.id), {
+ //       zoom: 12,
+//        center: location
+//      });
+//      var marker = new google.maps.Marker({
+//        position: location,
+//        map: map
+//      });
+//    }
     var map_key = this.props.map_key;
     return(
       <div>
-        <div id={"map-small_" + this.props.project.id} refs='mapCanvas' className='right'>I will be a map someday</div>
+        <div id={"map-small_" + this.props.project.id} refs='map_canvas' className='right'>I will be a map someday</div>
         <script async defer src={"https://maps.googleapis.com/maps/api/js?key=" + map_key + "&callback=initMap"}></script>
       </div>
     )
