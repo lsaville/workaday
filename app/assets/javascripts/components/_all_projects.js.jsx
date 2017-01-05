8f50a84b61d5c95bc90209a87191b64df44d79f8
Lee Saville <l.w.saville@gmail.com>
2017-01-05 10:17:24 -0700

    Add map function to component, not currently working

--- app/assets/javascripts/components/_all_projects.js.jsx
+++ app/assets/javascripts/components/_all_projects.js.jsx
@@ -8,14 +8,36 @@ var AllProjects = React.createClass({
   },
 
   render() {
+    var map = function initMap(project){
+      var location = {lat: project.lat, lng: project.lng} 
+      var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: location});
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+    };
+    var map_key = this.props.map_key;
     var projects = this.state.projects.map((project) => {
       return(
         <div key={project.id} className='project-card container z-depth-2'>
-          <h3>{project.title}</h3>
-          <p>lat: {project.lat}</p>
-          <p>lng: {project.lng}</p>
-          <div className='details'>{project.details}</div>
-          <div id='map-small' className=''></div>
+          <div className='card-content left'>
+            <h3>{project.title}</h3>
+            <p>lat: {project.lat}</p>
+            <p>lng: {project.lng}</p>
+            <div className='details'>{project.details}</div>
+          </div>
+          <div id='map-small' className='right'></div>
+          <script>
+            var map = function initMap(project){
+              var location = {lat: project.lat, lng: project.lng} 
+              var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: location});
+              var marker = new google.maps.Marker({
+                position: location,
+                map: map
+              });
+            };
+          </script>
+          <script async defer src={"https://maps.googleapis.com/maps/api/js?key=" + map_key + "&callback=initMap"}></script>
         </div>
       )
     }); 
