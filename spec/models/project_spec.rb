721b94be744f61c6c3cec2efe13076f93b1d0693
Lee Saville <l.w.saville@gmail.com>
2017-01-06 08:12:02 -0700

    Add validation testing for lat, lng, details, and title

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'validates lat' do
+      project = build(:project, lat: nil)
+
+      expect(project).to_not be_valid
+    end
+
+    it 'validates lng' do
+      project = build(:project, lng: nil)
+
+      expect(project).to_not be_valid
+    end
+
+    it 'validates details' do
+      project = build(:project, details: nil)
+
+      expect(project).to_not be_valid
+    end
+
+    it 'validates title' do
+      project = build(:project, title: nil)
+
+      expect(project).to_not be_valid
+    end
+  end
 end
