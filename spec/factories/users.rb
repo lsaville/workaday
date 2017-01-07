edd3b27be424561cea59bcfb798797238b9d43e2
Lee Saville <l.w.saville@gmail.com>
2017-01-07 16:08:09 -0700

    Add avatar_url to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     uid Faker::Number.number(10)
     name Faker::Name.last_name
     email Faker::Internet.email
+    avatar_url 'www.example.com'
     oauth_token Faker::Number.number(7)
     oauth_expires_at Faker::Time.forward(30, :morning)
   end
