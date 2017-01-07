561e390e5d230ed38b40634c926b4a7aa32a9b41
Lee Saville <l.w.saville@gmail.com>
2017-01-07 16:59:05 -0700

    Add create action for the creation of a project, add project_params method

--- app/controllers/api/v1/projects/projects_controller.rb
+++ app/controllers/api/v1/projects/projects_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::Projects::ProjectsController < ActionController::API
   def show
     render json: Project.find(params[:id])
   end
+
+  def create
+    project = Project.new(project_params)
+    project.save
+    render json: project, status: 201
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :details, :lat, :lng)
+    end
 end
