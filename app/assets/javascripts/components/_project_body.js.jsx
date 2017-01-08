67bc477938285824abd09ac82871001458d94237
Lee Saville <l.w.saville@gmail.com>
2017-01-08 14:01:35 -0700

    Add if statement to supply something while state is null

--- app/assets/javascripts/components/_project_body.js.jsx
+++ app/assets/javascripts/components/_project_body.js.jsx
@@ -1,18 +1,15 @@
 var ProjectBody = React.createClass({
-  getInitialState(){
-    return {project: null}
+  getInitialState() {
+    return { project: null }
   },
 
   componentDidMount() {
-    $.getJSON("/api/v1/projects/" + this.props.id, (response) => { this.setState({ project: response})});
-    debugger;
-  },
-
-  render() {
+    $.getJSON("/api/v1/projects/" + this.props.id, (response) => { this.setState({project: response})});
+    if (this.state.project === null) return(<div><h1>loading...</h1></div>)
     return(
       <div>
         <div>
-          <h1>project body once I learn about state</h1>
+          <h1>{this.state.project.title}</h1>
         </div>
         <div>
           <ProjectComments />
