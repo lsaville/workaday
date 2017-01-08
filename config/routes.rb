d7eede1bfd9e72a448760787f19b08fc4c71a52a
Lee Saville <l.w.saville@gmail.com>
2017-01-07 17:53:54 -0700

    Group the wildcards together

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :projects do
         get '',        to: 'projects#index'
-        get '/:id',    to: 'projects#show'
         post '',       to: 'projects#create'
+        get '/:id',    to: 'projects#show'
         put '/:id',    to: 'projects#update'
         patch '/:id',  to: 'projects#update'
         delete '/:id', to: 'projects#destroy'
