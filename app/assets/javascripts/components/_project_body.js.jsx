b1c1b4f4014dabe6e0deb89b280942b613683a16
Lee Saville <l.w.saville@gmail.com>
2017-01-08 12:21:30 -0700

    Add getInitialState and componentDidMount with api call. Add dummy ProjectComments component

--- app/assets/javascripts/components/_project_body.js.jsx
+++ app/assets/javascripts/components/_project_body.js.jsx
@@ -1,8 +1,22 @@
 var ProjectBody = React.createClass({
+  getInitialState(){
+    return {project: null}
+  },
+
+  componentDidMount() {
+    $.getJSON("/api/v1/projects/" + this.props.id, (response) => { this.setState({ project: response})});
+    debugger;
+  },
+
   render() {
     return(
       <div>
-        <h1>project body once I learn about state</h1>
+        <div>
+          <h1>project body once I learn about state</h1>
+        </div>
+        <div>
+          <ProjectComments />
+        </div>
       </div>
     )
   }
