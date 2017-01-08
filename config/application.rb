318132375dfd2ccf41a8dd537d04de7135441dee
Lee Saville <l.w.saville@gmail.com>
2017-01-07 18:24:31 -0700

    Add rack cors configuration allowing only local host and work-a-day heroku app through

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module Workaday
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000', '127.0.0.1.:3000', 'https://work-a-day.herokuapp.com'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
+      end
+    end
   end
 end
