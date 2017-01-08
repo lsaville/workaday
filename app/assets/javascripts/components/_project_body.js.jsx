4204a0d429a0495aef2c911e5cea8f249be4beb9
Lee Saville <l.w.saville@gmail.com>
2017-01-08 15:29:01 -0700

    Pass project lat and lng to Map component

--- app/assets/javascripts/components/_project_body.js.jsx
+++ app/assets/javascripts/components/_project_body.js.jsx
@@ -5,11 +5,15 @@ var ProjectBody = React.createClass({
 
   componentDidMount() {
     $.getJSON("/api/v1/projects/" + this.props.id, (response) => { this.setState({project: response})});
+  },
+
+  render() {
     if (this.state.project === null) return(<div><h1>loading...</h1></div>)
     return(
       <div>
-        <div>
+        <div className="container project">
           <h1>{this.state.project.title}</h1>
+          <Map lat={this.state.project.lat} lng={this.state.project.lng}/>
         </div>
         <div>
           <ProjectComments />
