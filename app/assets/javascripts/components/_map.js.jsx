63343848406eac1613bdfaf726b9bdf2b1a37198
Lee Saville <l.w.saville@gmail.com>
2017-01-09 14:05:08 -0700

    Add methods for the rendering of directions

--- app/assets/javascripts/components/_map.js.jsx
+++ app/assets/javascripts/components/_map.js.jsx
@@ -1,16 +1,22 @@
 var Map = React.createClass({
   componentDidMount() {
     this.location = new google.maps.LatLng(this.props.lat, this.props.lng);
+    this.louvre = new google.maps.LatLng(48.8596, 2.3369);
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
     this.map = this.createMap();
     this.marker = this.createMarker();
+    this.directions = this.createDirections();
   },
 
   createMap: function() {
     var mapOptions = {
-      zoom: 7,
+      zoom: 12,
       center: this.location
-    }
-    return new google.maps.Map(this.refs.map_canvas, mapOptions);
+    };
+    map = new google.maps.Map(this.refs.map_canvas, mapOptions);
+    this.directionsDisplay.setMap(map);
+    return map;
   },
 
   createMarker: function(){
@@ -19,6 +25,19 @@ var Map = React.createClass({
       map: this.map
     }
     return new google.maps.Marker(markerOptions);
+  },
+
+  createDirections: function() {
+    var request = {
+      origin: this.location,
+      destination: this.louvre,
+      travelMode: google.maps.TravelMode['DRIVING']
+    };
+    this.directionsService.route(request, (response, status) => {
+      if(status == 'OK') {
+        this.directionsDisplay.setDirections(response);
+      }
+    });
   },
 
   render(){
