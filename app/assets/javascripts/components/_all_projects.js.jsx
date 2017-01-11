54bf947b97c24dd10c9abbe2fa098927800efbfc
Lee Saville <l.w.saville@gmail.com>
2017-01-11 15:43:49 -0700

    Add link to project show

--- app/assets/javascripts/components/_all_projects.js.jsx
+++ app/assets/javascripts/components/_all_projects.js.jsx
@@ -13,7 +13,7 @@ var AllProjects = React.createClass({
       return(
         <div key={project.id} className='project-card container z-depth-2'>
           <div className='card-content left'>
-            <h3>{project.title}</h3>
+            <h3><a href={'/projects/' + project.id}>{project.title}</a></h3>
             <div className='details'>{project.details}</div>
           </div>
           <SmallMap project={project} />
