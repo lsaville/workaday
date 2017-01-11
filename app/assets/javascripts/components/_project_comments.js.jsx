61987658d4c19e20712864b3895edf8deef12a29
Lee Saville <l.w.saville@gmail.com>
2017-01-11 08:03:48 -0700

    Add container class to comments component. Add button to post comment

--- app/assets/javascripts/components/_project_comments.js.jsx
+++ app/assets/javascripts/components/_project_comments.js.jsx
@@ -1,8 +1,9 @@
 var ProjectComments = React.createClass({
   render() {
     return(
-      <div>
+      <div className="container">
         <h4>excellent comment section</h4>
+        <a href="#" className="waves-effect waves-light btn z-depth-2">Post Comment</a>
       </div>
     )
   }
