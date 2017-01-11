a7310a31f0439cef43d61ca923ccffe7542579f8
Lee Saville <l.w.saville@gmail.com>
2017-01-11 08:02:28 -0700

    Remove unecessary code from proof of concept. Add a field for input and 'instantiate' the new js AutocompleteDirecitionsHandler

--- app/assets/javascripts/components/_map.js.jsx
+++ app/assets/javascripts/components/_map.js.jsx
@@ -1,22 +1,18 @@
 var Map = React.createClass({
   componentDidMount() {
     this.location = new google.maps.LatLng(this.props.lat, this.props.lng);
-    this.louvre = new google.maps.LatLng(48.8596, 2.3369);
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsDisplay = new google.maps.DirectionsRenderer();
     this.map = this.createMap();
     this.marker = this.createMarker();
-    this.directions = this.createDirections();
+    new AutocompleteDirectionsHandler(this);
   },
 
   createMap: function() {
     var mapOptions = {
+      mapTypeControl: false,
       zoom: 12,
       center: this.location
     };
     map = new google.maps.Map(this.refs.map_canvas, mapOptions);
-    this.directionsDisplay.setMap(map);
-    this.directionsDisplay.setPanel(this.refs.map_panel);
     return map;
   },
 
@@ -28,23 +24,11 @@ var Map = React.createClass({
     return new google.maps.Marker(markerOptions);
   },
 
-  createDirections: function() {
-    var request = {
-      origin: this.location,
-      destination: this.louvre,
-      travelMode: google.maps.TravelMode['DRIVING']
-    };
-    this.directionsService.route(request, (response, status) => {
-      if(status == 'OK') {
-        this.directionsDisplay.setDirections(response);
-      }
-    });
-  },
-
   render(){
     return(
       <div>
-        <div id={'map'} ref='map_canvas' className='z-depth-2'>I will be a map someday!</div>
+        <input id="origin-input" ref="origin_input" className="controls input-field" type="text" placeholder="Get Directions" />
+        <div id={'map'} ref='map_canvas' className='z-depth-2'></div>
         <div id='right-panel' ref='map_panel' className='z-depth-2'></div>
       </div>
     )
